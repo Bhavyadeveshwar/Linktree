beae06022fd556c14ebd0cd18c7ec5857aef3b7c
Bhavyadeveshwar <bhavyadeveshwar@gmail.com>
2024-07-26 16:31:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import Links from './Components/Links/Links'
+import { LampDemo } from './Components/components/ui/lamp'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <LampDemo/>
+    <Links/>
+    
+    
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Links/Links.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import Instagram from '../../assets/instagram.png'
+
+
+const Links = () => {
+  return (
+    
+      
+    <div className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-slate-950 w-full rounded-md z-0">
+      
+      
+      <div className="grid grid-col-7 ">
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-white hover:bg-gray-100 text-purple-900 font-bold py-2 px-48 rounded mx-32 mb-4"
+        > <div className='flex flex-wrap justify-center items-center'>
+          <img src={Instagram} alt="" className='w-10 h-10 mr-4'/>The Ace'24 | Instagram </div>
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-white hover:bg-gray-100 text-purple-900 font-bold py-2 px-48 rounded mx-32 mb-4"
+        > <div className='flex flex-wrap justify-center items-center'>
+          <img src={Instagram} alt="" className='w-10 h-10 mr-4'/>The Ace'24 | Instagram </div>
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-white hover:bg-gray-100 text-purple-900 font-bold py-2 px-48 rounded mx-32 mb-4"
+        > <div className='flex flex-wrap justify-center items-center'>
+          <img src={Instagram} alt="" className='w-10 h-10 mr-4'/>The Ace'24 | Instagram </div>
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-white hover:bg-gray-100 text-purple-900 font-bold py-2 px-48 rounded mx-32 mb-4"
+        > <div className='flex flex-wrap justify-center items-center'>
+          <img src={Instagram} alt="" className='w-10 h-10 mr-4'/>The Ace'24 | Instagram </div>
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-white hover:bg-gray-100 text-purple-900 font-bold py-2 px-48 rounded mx-32 mb-4"
+        > <div className='flex flex-wrap justify-center items-center'>
+          <img src={Instagram} alt="" className='w-10 h-10 mr-4'/>The Ace'24 | Instagram </div>
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-white hover:bg-gray-100 text-purple-900 font-bold py-2 px-48 rounded mx-32 mb-4"
+        > <div className='flex flex-wrap justify-center items-center'>
+          <img src={Instagram} alt="" className='w-10 h-10 mr-4'/>The Ace'24 | Instagram </div>
+        </a>
+        
+      </div>
+    </div>
+    
+  );
+};
+
+export default Links;
+
